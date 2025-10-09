925f619e4cc0546c02d054f6063211a13012facf
Aidar I. <aidarisenov31@gmail.com>
2025-10-09 19:26:41 +0500

    hz, character add take_damage method

--- practice_6/Character.java
+++ practice_6/Character.java
@@ -1,6 +1,36 @@
 public abstract class Character {
     protected String name;
     protected int health;
-    protected AttackStrategy attack_strategy;  
-    protected GameObserver game_observer;
+    protected AttackStrategy attack_strategy;
+    // я хз там вроде не написано
+    // можно по идее List Observer если много реализаций пока так
+    protected GameObserver game_observer; 
+
+
+    public Character(String name, int health, AttackStrategy strategy, GameObserver observer) {
+        this.name = name;
+        this.health = health;
+        this.attack_strategy = strategy;
+        this.game_observer = observer;
+    }
+
+    // геттеры 
+    public String getName() { return name;}
+    public int getHealth() { return health;}
+    public boolean isAlive() { return health > 0;}
+    
+    public void takeDamage(int damage) {
+        if (!isAlive()) return;
+        health -= damage;
+        if (health <= 0) {
+            health = 0;
+            // todo: вынести в отдельный метод =) 
+            game_observer.onEvent("[LOG] " + name + "погиб.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " [HP=" + health + "]";
+    }
 }
